63f836b0e203efaa518442e51c11053f6ab58eab
mostapko <github@ostapko.com>
2022-11-18 13:17:44 +0100

    prettier imports

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import {HomeRoutingModule} from "./home/home-routing.module";
-import {TranslateModule} from "@ngx-translate/core";
+import { HomeRoutingModule } from "./home/home-routing.module";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   declarations: [
