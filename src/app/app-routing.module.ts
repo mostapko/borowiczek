7fa985e867a2b6514b76265c0145acacb937b6a8
mostapko <github@ostapko.com>
2022-11-18 13:17:29 +0100

    localise and very basic home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  LocalizeParser,
+  LocalizeRouterModule,
+  LocalizeRouterSettings,
+  ManualParserLoader
+} from "@gilsdav/ngx-translate-router";
+import { TranslateService } from "@ngx-translate/core";
+import { Location } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then((mod) => mod.HomeModule) },
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    LocalizeRouterModule.forRoot(routes, {
+      parser: {
+        provide: LocalizeParser,
+        useFactory: (translate: any, location: any, settings: any) =>
+          new ManualParserLoader(translate, location, settings, ['pl','en'], 'ROUTES.'),
+        deps: [TranslateService, Location, LocalizeRouterSettings, HttpClient]
+      }
+    }),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './logic/reducers';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/locales/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -12,6 +18,15 @@ import { reducers, metaReducers } from './logic/reducers';
   ],
   imports: [
     BrowserModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'pl',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
+    HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
     StoreModule.forRoot(reducers, {
